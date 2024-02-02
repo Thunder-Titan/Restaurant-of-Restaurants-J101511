601636ac5e6759d1fa6ee32ccef0047387b0a56f
2204042 <2204042@chester.ac.uk>
2024-02-02 14:51:03 +0000

    Created the basket system-ish. Some flaws present in its functionality

---
+++ Data/CheckoutCustomer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantofRestaurants.Data
+{
+    public class CheckoutCustomer
+    {
+        [Key]
+        [StringLength(50)]
+        public string Email { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        public int BasketID { get; set; }
+    }
+    public class Basket
+    {
+        [Key]
+        public int BasketID { get; set; }
+    }
+    public class BasketItem
+    {
+        [Required]
+        public int StockID { get; set; }
+        [Required]
+        public int BasketID { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
--- Pages/Menu/Index.cshtml.cs
+++ Pages/Menu/Index.cshtml.cs
@@ -7,15 +7,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RestaurantofRestaurants.Data;
 using RestaurantofRestaurants.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace RestaurantofRestaurants.Pages.Menu
 {
     public class IndexModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RestaurantofRestaurantsContext _db;
         private readonly RestaurantofRestaurants.Data.RestaurantofRestaurantsContext _context;
-
-        public IndexModel(RestaurantofRestaurants.Data.RestaurantofRestaurantsContext context)
+        public IndexModel(RestaurantofRestaurantsContext db, UserManager<IdentityUser> userManager, RestaurantofRestaurants.Data.RestaurantofRestaurantsContext context)
         {
+            _db = db;
+            _userManager = userManager;
             _context = context;
         }
 
@@ -27,6 +31,46 @@ namespace RestaurantofRestaurants.Pages.Menu
             {
                 FoodItem = await _context.FoodItems.ToListAsync();
             }
+        }
+
+        public async Task<IActionResult> OnPostBuyAsync(int itemID)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            CheckoutCustomer customer = await _db
+            .CheckoutCustomers
+            .FindAsync(user.Email);
+
+            var item = _db.BasketItem
+                .FromSqlRaw("SELECT * FROM BasketItems WHERE StockID = {0}" + 
+                "AND BasketID = {1}", itemID, customer.BasketID)
+                       .ToList()
+                       .FirstOrDefault();
+
+            if (item == null) 
+            {
+                BasketItem newItem = new BasketItem
+                {
+                    BasketID = customer.BasketID,
+                    StockID = itemID,
+                    Quantity = 1
+                };
+                _db.BasketItem.Add(newItem);
+                await _db.SaveChangesAsync();
+            }
+            else
+            {
+                item.Quantity = item.Quantity + 1;
+                _db.Attach(item).State = EntityState.Modified;
+                try
+                {
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException e) 
+                {
+                    throw new Exception($"Basket not found!", e);
+                }
+            }
+            return RedirectToPage();
         }
     }
 }
