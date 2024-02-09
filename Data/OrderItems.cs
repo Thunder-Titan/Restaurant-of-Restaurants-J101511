0b591efcaaa4a9cf4976d4bdbe2a5efceafadd30
2204042 <2204042@chester.ac.uk>
2024-02-09 14:31:44 +0000

    Partway through creating the checkout system, but it doesn't work for some fucking reason

---
+++ Data/OrderItems.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantofRestaurants.Data
+{
+    public class OrderItems
+    {
+        [Required]
+        public int OrderNo { get; set; }
+        [Required]
+        public int StockID { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
--- Data/RestaurantofRestaurantsContext.cs
+++ Data/RestaurantofRestaurantsContext.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using RestaurantofRestaurants.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RestaurantofRestaurants.Data
 {
@@ -20,6 +21,11 @@ namespace RestaurantofRestaurants.Data
         public DbSet<CheckoutCustomer> CheckoutCustomers { get; set; } = default!;
         public DbSet<Basket> Baskets { get; set; } = default!;
         public DbSet<BasketItem> BasketItem { get; set; } = default!;
+        public DbSet<OrderHistory> OrderHistories { get; set; } = default!;
+        public DbSet<OrderItems> OrderItems { get; set; } = default!;
+
+        [NotMapped]
+        public DbSet<CheckoutItems> CheckoutItems { get; set; } = default!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -27,6 +33,7 @@ namespace RestaurantofRestaurants.Data
             modelBuilder.Entity<FoodItem>().ToTable("FoodItem");
 
             modelBuilder.Entity<BasketItem>().HasKey(t => new { t.StockID, t.BasketID });
+            modelBuilder.Entity<OrderItems>().HasKey(t => new { t.OrderNo, t.StockID });
         }
     }
 }
